ce42132295317294107b13d1c5c462d6d195581f
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-21 11:00:17 -0600

    added background color

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,27 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-let connection;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    // NOTE: Be sure to add your MySQL password here!
-    password: "password",
-    database: "burger_db",
-  });
-}
+// let connection;
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     // NOTE: Be sure to add your MySQL password here!
+//     password: "password",
+//     database: "burger_db",
+//   });
+// }
 
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "password",
-//   database: "burger_db",
-// });
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burger_db",
+});
 
 connection.connect((err) => {
   if (err) {
